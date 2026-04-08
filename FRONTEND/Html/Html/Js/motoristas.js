73659896063bc6af1935a6f14cc11bc17408af6d
murillo8818-glitch <murillo8818@gmail.com>
2026-04-08 15:45:16 -0300

    Organizção teste

---
+++ FRONTEND/Html/Html/Js/motoristas.js
@@ -0,0 +1,24 @@
+document.getElementById('open_btn').addEventListener('click',function () {
+    document.getElementById('sidebar').classList.toggle('open-sidebar');     
+});
+/* 
+const amrzenarCadastros = []; 
+var nome; 
+
+class motororistas {
+
+  //  informações pessoais 
+
+CPF = this.cpf; 
+NOME = this.nome;  
+DATA_NASC = this.data_nasc; 
+
+}
+function motoristaF( motoristas) {
+    (CPF,NOME,DATA_NASC);
+
+} */
+    
+
+
+
